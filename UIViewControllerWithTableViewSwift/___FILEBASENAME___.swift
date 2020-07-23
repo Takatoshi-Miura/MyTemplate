2596e249430ee6c8768fe9969ce14d5745f920ba
Takatoshi-Miura <takatoshi052031@gmail.com>
2020-07-23 17:37:55 +0900

    [Add] UIViewControllerWithTableViewSwiftを追加定義。

---
+++ UIViewControllerWithTableViewSwift/___FILEBASENAME___.swift
@@ -0,0 +1,35 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+
+    //MARK:- ライフサイクルメソッド
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    
+    
+    //MARK:- 変数の宣言
+    
+    
+    
+    //MARK:- UIの設定
+    
+    
+    
+    
+    //MARK:- 画面遷移
+    
+    
+    
+    //MARK:- その他のメソッド
+
+
+}
